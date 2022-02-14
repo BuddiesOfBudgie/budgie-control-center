dcef7472a4b543e1051ecae0dd9350de01c157af
David Mohammed <fossfreedom@ubuntu.com>
2022-02-14 18:48:20 +0000

    v0.4 release

--- mkrelease.sh
+++ mkrelease.sh
@@ -4,7 +4,7 @@ set -e
 git submodule init
 git submodule update
 
-VERSION="0.3"
+VERSION="0.4"
 NAME="budgie-control-center"
 git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
