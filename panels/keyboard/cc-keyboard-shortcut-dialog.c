Farmer-Markus/gnome-control-center

keyboard: Avoid segfault due to no "data" for `custom_shortcut_add_row`
Ultimately it would be good to get rid of the use of
`g_object_set_data`/`g_object_get_data` here, but this works for now.

Fixes https://gitlab.gnome.org/GNOME/gnome-control-center/-/issues/1373.

panels/keyboard/cc-keyboard-shortcut-dialog.c
@@ -235,7 +235,7 @@ remove_item (CcKeyboardShortcutDialog *self,
 
       row_data = g_object_get_data (G_OBJECT (child), "data");
 
-      if (row_data->item == item)
+      if (row_data && row_data->item == item)
         {
           if (strcmp (row_data->section_id, "custom") == 0)
             {
@@ -410,6 +410,9 @@ reset_shortcut (CcKeyboardShortcutDialog *self,
                 GtkWidget                *row)
 {
   ShortcutRowData *data;
+
+  if (row == GTK_WIDGET (self->custom_shortcut_add_row))
+    return;
 
   data = g_object_get_data (G_OBJECT (row), "data");
 

BuddiesOfBudgie/budgie-control-center

panels/keyboard/cc-keyboard-shortcut-dialog.c
@@ -234,7 +234,7 @@ remove_item (CcKeyboardShortcutDialog *self,
 
       row_data = g_object_get_data (l->data, "data");
 
-      if (row_data->item == item)
+      if (row_data && row_data->item == item)
         {
           if (strcmp (row_data->section_id, "custom") == 0)
             {
@@ -402,6 +402,9 @@ reset_all_shortcuts_cb (GtkWidget *widget,
   ShortcutRowData *data;
 
   self = user_data;
+
+  if (widget == GTK_WIDGET (self->custom_shortcut_add_row))
+    return;
 
   data = g_object_get_data (G_OBJECT (widget), "data");
 