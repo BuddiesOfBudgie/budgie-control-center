9739f0b69664fa4c2d88f709b86dcc530a8d2972
David Mohammed <fossfreedom@ubuntu.com>
2022-08-13 22:22:44 +0100

    Add transifex and translation script support

---
+++ scripts/updatePot.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function do_gettext()
+{
+    xgettext --package-name=budgie-control-center --package-version=1.1.0 $* --default-domain=budgie-control-center --join-existing --from-code=UTF-8 --no-wrap
+}
+
+function do_intltool()
+{
+    intltool-extract --type=$1 $2
+}
+
+rm budgie-control-center.po -f
+touch budgie-control-center.po
+
+#for file in `find src -not -path '*/gvc/*' -name "*.c" -or -name "*.vala"`; do
+for file in `find . -name "*.c"`; do
+    if [[ `grep -F "_(\"" $file` ]]; then
+        echo $file
+        do_gettext $file --add-comments
+    fi
+done
+
+for file in `find . -name "*.ui"`; do
+    if [[ `grep -F "translatable=\"yes\"" $file` ]]; then
+        do_intltool gettext/glade $file
+        do_gettext ${file}.h --add-comments --keyword=N_:1
+        rm $file.h
+    fi
+done
+
+for file in `find . -name "*.in"`; do
+    if [[ `grep -E "^_*" $file` ]]; then
+        do_intltool gettext/keys $file
+        do_gettext ${file}.h --add-comments --keyword=N_:1
+        rm $file.h
+    fi
+done
+
+mv budgie-control-center.po po/budgie-control-center.pot
