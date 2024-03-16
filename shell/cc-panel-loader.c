150a28287fb3ed150543fa432407cf0025ebc2b3
David Mohammed <fossfreedom@ubuntu.com>
2024-03-16 21:08:42 +0000

    Make the Location panel an optional panel Mozilla will deprecate its service at the end of March 2024. Thus location support will be broken until a replacement solution is found. https://github.com/mozilla/ichnaea/issues/2065

--- shell/cc-panel-loader.c
+++ shell/cc-panel-loader.c
@@ -67,7 +67,9 @@ extern GType cc_wacom_panel_get_type (void);
 #ifdef BUILD_WWAN
 extern GType cc_wwan_panel_get_type (void);
 #endif /* BUILD_WWAN */
+#ifdef HAVE_LOCATION
 extern GType cc_location_panel_get_type (void);
+#endif /* HAVE_LOCATION*/
 extern GType cc_camera_panel_get_type (void);
 extern GType cc_microphone_panel_get_type (void);
 extern GType cc_usage_panel_get_type (void);
@@ -111,7 +113,9 @@ static CcPanelLoaderVtable default_panels[] =
   PANEL_TYPE("display",          cc_display_panel_get_type,              NULL),
   PANEL_TYPE("info-overview",    cc_info_overview_panel_get_type,        NULL),
   PANEL_TYPE("keyboard",         cc_keyboard_panel_get_type,             NULL),
+#ifdef HAVE_LOCATION
   PANEL_TYPE("location",         cc_location_panel_get_type,             NULL),
+#endif
   //PANEL_TYPE("microphone",       cc_microphone_panel_get_type,           NULL),
   PANEL_TYPE("mouse",            cc_mouse_panel_get_type,                NULL),
   PANEL_TYPE("multitasking",     cc_multitasking_panel_get_type,         NULL),
