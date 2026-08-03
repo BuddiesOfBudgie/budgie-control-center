74784013472704bbc8f42190e8bcd20967c3d3bc
David Mohammed <fossfreedom@ubuntu.com>
2026-08-03 18:53:41 +0100

    Bump microrelease

--- scripts/mkRelease.sh
+++ scripts/mkRelease.sh
@@ -4,7 +4,7 @@ set -e
 git submodule init
 git submodule update
 
-VERSION="2.1.1"
+VERSION="2.1.2"
 NAME="budgie-control-center"
 git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
