971ebb7a3e12aeb58b1a036ebd0cc7f2a0e7eb4e
David Mohammed <fossfreedom@ubuntu.com>
2026-08-18 18:51:29 +0100

    Bump release version, latest translations

--- scripts/mkRelease.sh
+++ scripts/mkRelease.sh
@@ -4,7 +4,7 @@ set -e
 git submodule init
 git submodule update
 
-VERSION="2.1.2"
+VERSION="2.1.3"
 NAME="budgie-control-center"
 git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
